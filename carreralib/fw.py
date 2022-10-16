207c4ece09caf15d56ce64680eb47b85911dd140
Jeff Gross <jeff@grosssoftware.com>
2022-10-16 18:33:32 -0400

    On display version if no update file.

--- carreralib/fw.py
+++ carreralib/fw.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(prog='python -m carreralib.fw')
     parser.add_argument('device', metavar='DEVICE')
-    parser.add_argument('updatefile', metavar='FILE')
-    parser.add_argument('-l', '--logfile', nargs="?", default='carreralib.log', const=None)
+    parser.add_argument('updatefile', metavar='FILE', nargs='?', default=None)
+    parser.add_argument('-l', '--logfile', nargs='?', default='carreralib.log', const=None)
     parser.add_argument('-t', '--timeout', default=1.0, type=float)
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
@@ -21,4 +21,6 @@ if __name__ == "__main__":
     with contextlib.closing(ControlUnit(args.device, timeout=args.timeout)) as cu:
         print('CU version %s' % cu.version().decode())
 
-        cu.updatefw(args.updatefile)
+        if args.updatefile:
+            cu.updatefw(args.updatefile)
+
